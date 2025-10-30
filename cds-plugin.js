fb88341ed7380ba8270ee7db956fa3a6836ee655
Luca Fischer <luca.fischer@sap.com>
2025-10-30 16:52:14 +0100

    Change condition to check for test environments

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,5 +1,5 @@
-// Prevents CAP console plugin from initializing during Jest test runs.
-if (process.env.NODE_ENV !== 'test' || !process.env.JEST_WORKER_ID) {
+// Prevents CAP console plugin from initializing during test runs.
+if (process.env.NODE_ENV !== 'test') {
   import('@sap/cds').then(({ default: cds }) => {
     cds.on('served', async () => {
       const { default: main } = await import('./dist/index.js');
