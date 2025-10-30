3ec9df1056791d1c12fc571e012d5b27597265f2
Luca Fischer <luca.fischer@sap.com>
2025-10-30 16:27:55 +0100

    Conditionally initialize plugin

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,5 +1,9 @@
-import cds from '@sap/cds';
-
-import main from './dist/index.js';
-
-cds.on('served', main);
+// Prevents CAP console plugin from initializing during Jest test runs.
+if (process.env.NODE_ENV !== 'test' || !process.env.JEST_WORKER_ID) {
+  import('@sap/cds').then(({ default: cds }) => {
+    cds.on('served', async () => {
+      const { default: main } = await import('./dist/index.js');
+      main();
+    });
+  });
+}
